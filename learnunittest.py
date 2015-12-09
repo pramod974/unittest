f825be290c2c492789b517116803274e6d30535d
pramod <pramod974@gmail.com>
2015-12-09 16:47:23 +0530

    master branch changes

--- learnunittest.py
+++ learnunittest.py
@@ -2,7 +2,10 @@ __author__ = 'Pramod.Kumar'
 
 import unittest
 import pandas
+#pandas is not lovely !
 # Here's our "unit".
+def justgit():
+    print "git !!!"
 def IsOdd(n):
     return n % 2 == 1
 
